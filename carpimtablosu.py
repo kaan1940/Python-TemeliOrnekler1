cf827401124046131512f5c50fe17458890791c5
KAAN BAL <balkaan01@gmail.com>
2022-04-14 17:21:52 +0300

    ic ice for ile carpim tablosu

---
+++ carpimtablosu.py
@@ -0,0 +1,5 @@
+#######ic ice for donguleri kullanarak carpim tablosu yapildi######
+for i in range(1,6):
+    for j in range(1,11):
+        carpim = i * j
+        print(str(i) + "X" + str(j) + "=" + str(carpim))
