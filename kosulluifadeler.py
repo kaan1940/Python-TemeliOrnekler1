265a9a09259123fac65b2a096f390c70fb11ee18
KAAN BAL <balkaan01@gmail.com>
2022-04-14 15:01:24 +0300

    kosullu ifadelere baslandi

---
+++ kosulluifadeler.py
@@ -0,0 +1,6 @@
+########kosullu ifadeler - ehliyet alabilme
+yas = int(input())
+if yas >= 18:
+    print("ehliyet alabilir")
+else:
+    print("ehilyet alamaz")
---
+++ pozitifnegatif.py
@@ -0,0 +1,7 @@
+sayi = int(input())
+if sayi > 0:
+    print("sayi pozitiftir")
+elif sayi < 0:
+    print("sayi negatiftir")
+else:
+    print("sayi sifira esittir")
