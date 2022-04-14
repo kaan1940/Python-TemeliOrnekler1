d7ccebc99492de9d792f04446014ff9966749180
coders clup <hallo@codersclup.com>
2022-04-14 14:26:36 +0300

    alancevrehesaplandi

---
+++ alancevre.py
@@ -0,0 +1,7 @@
+#dikdortgen alan ve cevre hesabi
+kisakenar = int(input())
+uzunkenar = int(input())
+alan = kisakenar * uzunkenar
+cevre = (2 * kisakenar) + (2 * uzunkenar)
+print(alan)
+print(cevre)
